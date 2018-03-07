4c850df467e25bf5b9e657b54f54194c9d993202
xushaodong <zijingyuanyuan@163.com>
2018-03-07 15:32:06 +0800

    更新了userbean

--- app/src/main/java/com/sixrooms/testcmake/Test.java
+++ app/src/main/java/com/sixrooms/testcmake/Test.java
@@ -1,13 +1,18 @@
 package com.sixrooms.testcmake;
 
 /**
- * 类描述：
+ * 类描述：测试
  *
  * @Author 许少东
  * Created at 2017/12/18.
  */
 
 public class Test {
+    private String uid;
+
+    public Test(String uid) {
+
+    }
 
 
 }
---
+++ app/src/main/java/com/sixrooms/testcmake/UserBean.java
@@ -0,0 +1,11 @@
+package com.sixrooms.testcmake;
+
+/**
+ * 类描述：
+ *
+ * @Author 许少东
+ * Created at 2018/3/7.
+ */
+
+public class UserBean {
+}
